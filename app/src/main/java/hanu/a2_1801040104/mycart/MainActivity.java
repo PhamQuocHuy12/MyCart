7bc615426b2120e47ec4c488842aeaf83be799b7
PhamQuocHuy12 <1801040104@s.hanu.edu.vn>
2021-05-10 20:58:59 +0700

    Initial commit

---
+++ app/src/main/java/hanu/a2_1801040104/mycart/MainActivity.java
@@ -0,0 +1,14 @@
+package hanu.a2_1801040104.mycart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
